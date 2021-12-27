deff8036b60fbd1e627f1726aeab5043441e1283
Daniel Rodas <danielangelrs@gmail.com>
2021-12-26 21:21:41 -0500

    test: docker Actions

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,12 +2,12 @@ const core = require("@actions/core");
 const github = require('@actions/github');
 
 try {
-//   throw new Error("some error message");
+  //   throw new Error("some error message");
 
   core.debug('Debug message');
   core.warning('Warning message');
   core.error('Error message');
-  
+
   const name = core.getInput("who-to-greet");
   core.setSecret(name);
   console.log(`Hello ${name}`);
@@ -19,7 +19,6 @@ try {
   console.log(JSON.stringify(github, null, "\t"));
   core.endGroup();
   core.exportVariable('HELLO', "hello")
-} catch (error)
-{
+} catch (error) {
   core.setFailed(error.message);
 }
