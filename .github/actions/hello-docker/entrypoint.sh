9b912f1d656e06ceae85202df5f445a41193b165
Daniel Rodas <danielangelrs@gmail.com>
2021-12-26 21:29:00 -0500

    test: docker Actions

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -7,7 +7,7 @@ echo "::error  ::Error Message"
 echo "::add-mask::$1"
 echo "Hello $1"
 time=$(date)
-echo "::set-output name=time:$time"
+echo "::set-output name=time::$time"
 
 echo "::group::Some expandable logs"
 echo 'some stuff'
