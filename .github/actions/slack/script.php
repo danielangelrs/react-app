bb0421712505f8dcffc31b5dac8a8c09e26984eb
Daniel Rodas <danielangelrs@gmail.com>
2021-12-27 14:29:41 -0500

    test: php

--- .github/actions/slack/script.php
+++ .github/actions/slack/script.php
@@ -3,81 +3,54 @@
 require_once 'vendor/autoload.php';
 Requests::register_autoloader();
 
+// var_dump($argv);
+// var_dump($_ENV);
+
 echo "::debug ::Sending a request to slack\n";
 
 $response = Requests::post(
-    $_ENV ["INPUT_SLACK_WEBHOOK"],
+    $_ENV['INPUT_SLACK_WEBHOOK'],
     array(
         'Content-type' => 'application/json'
     ),
     json_encode(array (
         'blocks' => 
-        array (
-          0 => 
-          array (
-            'type' => 'section',
-            'text' => 
             array (
-              'type' => 'mrkdwn',
-              'text' => $_ENV ["INPUT_MESSAGE"],
+                array (
+                    "type" => "section",
+                    "text" => array (
+                        "type" => "mrkdwn",
+                        "text" => $_ENV['INPUT_MESSAGE'],
+                    ),
+                ),
+                array (
+                    "type" => "section",
+                    "fields" => array (
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",
+                        ),
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*Event:*\n{$_ENV['GITHUB_EVENT_NAME']}",
+                        ),
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*Ref:*\n{$_ENV['GITHUB_REF']}",
+                        ),
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*SHA:*\n{$_ENV['GITHUB_SHA']}",
+                        ),
+                    ),
+                ),
             ),
-          ),
-          1 => 
-          array (
-            'type' => 'section',
-            'fields' => 
-            array (
-              0 => 
-              array (
-                'type' => 'mrkdwn',
-                'text' => '*Repository:*
-      {$_ENV ['GITHUB_REPOSITORY']}',
-              ),
-              1 => 
-              array (
-                'type' => 'mrkdwn',
-                'text' => '*Event:*
-      {$_ENV ['GITHUB_EVENT_NAME']}',
-              ),
-              array (
-                'type' => 'mrkdwn',
-                'text' => '*Ref:*
-      {$_ENV ['GITHUB_GITHUB_REF']}',
-              ),
-              array (
-                'type' => 'mrkdwn',
-                'text' => '*SHA:*
-      {$_ENV ['GITHUB_SHA']}',
-              ),
-              2 => 
-              array (
-                'type' => 'mrkdwn',
-                'text' => '*Last Update:*
-      Mar 10, 2015 (3 years, 5 months)',
-              ),
-              3 => 
-              array (
-                'type' => 'mrkdwn',
-                'text' => '*Reason:*
-      All vowel keys aren\'t working.',
-              ),
-              4 => 
-              array (
-                'type' => 'mrkdwn',
-                'text' => '*Specs:*
-      "Cheetah Pro 15" - Fast, really fast"',
-              ),
-            ),
-          ),
-        ),
-      ))
-     
+    ))
 );
 
-
-echo "::group::Slack response\n";
+echo "::group::Slack Reponse\n";
 var_dump($response);
-echo '::endgroup::\n';
+echo "::endgroup::\n";
 
 if(!$response->success) {
     echo $response->body;
